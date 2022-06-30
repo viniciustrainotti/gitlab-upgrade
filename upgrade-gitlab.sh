dfdd802f93827c5e1c38ed861e97b1e3a001c058
viniciustrainottiobjective <93720413+viniciustrainottiobjective@users.noreply.github.com>
2022-06-30 08:23:17 -0300

    Change command upgrade to install
    Change command upgrade to install

--- upgrade-gitlab.sh
+++ upgrade-gitlab.sh
@@ -5,7 +5,7 @@ UPGRADE_VERSION=$1
 echo $UPGRADE_VERSION
 
 echo "-- upgrade --"
-docker exec -u root gitlab2 bash -c "apt upgrade gitlab-ce=$UPGRADE_VERSION -y"
+docker exec -u root gitlab2 bash -c "apt install gitlab-ce=$UPGRADE_VERSION -y"
 
 echo "-- restart gitlab --"
 docker exec -u root gitlab2 bash -c "gitlab-ctl restart"
